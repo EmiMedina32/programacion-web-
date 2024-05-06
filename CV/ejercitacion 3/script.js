e4839c7488d3878d52ce8247c35b74b86911d35d
emimedina32 <emilianomedina357@gmail.com>
2024-05-06 15:49:49 -0300

    no se

---
+++ CV/ejercitacion 3/script.js
@@ -0,0 +1,7 @@
+function agregarElemento(){
+
+    let lista = document.getElementById("lista");
+    let input = document.getElementById(textarea).ariaValueMax
+    const button  = document.createElement("button");
+    //console.log(lista);
+}
